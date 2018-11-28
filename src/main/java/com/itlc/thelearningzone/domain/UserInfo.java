d82c42aa5764310bd5fe2483e947fbcb66d28e42
Shaun Conroy <d00187490@student.dkit.ie>
2018-11-28 23:42:38 +0000

    Fixed: UserInfo liquibase config, removed generated value from UserInfo ID

--- src/main/java/com/itlc/thelearningzone/domain/UserInfo.java
+++ src/main/java/com/itlc/thelearningzone/domain/UserInfo.java
@@ -23,7 +23,6 @@ public class UserInfo implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "tutor_skills")
