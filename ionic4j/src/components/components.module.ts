268fbea27e6bdefe9902128d2019ab80eb9e198c
Shaun Conroy <d00187490@student.dkit.ie>
2018-12-06 18:34:49 +0000

    Separated front-end and back-end

--- ionic4j/src/components/components.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { TopMenuComponent } from './top-menu/top-menu';
-import { IonicModule } from 'ionic-angular';
-@NgModule({
-	declarations: [TopMenuComponent],
-	imports: [IonicModule],
-	exports: [TopMenuComponent]
-})
-export class ComponentsModule {}
