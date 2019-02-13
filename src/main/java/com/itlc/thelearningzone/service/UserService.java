99cb01cee442cfbf80f8383665eee82eebeeb986
Shaun Conroy <shaunconroy17@gmail.com>
2019-02-13 22:00:44 +0000

    Bug fix

--- src/main/java/com/itlc/thelearningzone/service/UserService.java
+++ src/main/java/com/itlc/thelearningzone/service/UserService.java
@@ -263,7 +263,8 @@ public class UserService {
 
     // Added delete UserInfo functionality before parent entity (User) is deleted
     public void deleteUser(String login) {
-    	if (userRepository.findOneByLogin(login).isPresent()) {
+    	Optional<User> searchUser = userRepository.findOneByLogin(login);
+    	if (searchUser.isPresent()) {
     		userInfoService.findOne(userRepository.findOneByLogin(login).get().getId()).ifPresent(userInfo -> {
                 userInfoService.delete(userInfo.getId());
                 log.debug("Deleted UserInfo: {}", userInfo);
@@ -277,27 +278,6 @@ public class UserService {
     		throw new IllegalArgumentException("User login does not exist");
     	}
     }
-    
- // Added delete UserInfo functionality before parent entity (User) is deleted
-//    public void deleteUser(String login) {
-//    	Optional<User> user = userRepository.findOneByLogin(login);
-//    	if (user.isPresent()) {
-//    		Optional<UserInfo> userInfo = userInfoRepository.findById(user.get().getId());
-//    		if (userInfo.isPresent())
-//    		{	// Delete the UserInfo
-//    			userInfoService.delete(userInfo.get().getId());
-//                log.debug("Deleted UserInfo: {}", userInfo);
-//                // Delete the User and clear caches
-//                userRepository.deleteById(user.get().getId());
-//                this.clearUserCaches(user.get());
-//                log.debug("Deleted User: {}", user);
-//    		} else {
-//        		throw new IllegalArgumentException("UserInfo ID does not exist");
-//        	}
-//    	} else {
-//    		throw new IllegalArgumentException("User login does not exist");
-//    	}
-//    }
 
     public void changePassword(String currentClearTextPassword, String newPassword) {
         SecurityUtils.getCurrentUserLogin()
